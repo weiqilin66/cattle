f4bcb4d2f4feab8eef3a358e054c11a3800f3f7b
weiqilin66 <weiqilin66@gmail.com>
2021-05-29 11:13:42 +0800

    nacos+springboot范例

---
+++ Me/Q/run_test/src/main/java/org/wayne/test/t1/RunMain.java
@@ -0,0 +1,11 @@
+package org.wayne.test.t1;
+
+/**
+ * @TODO:
+ * @author: lwq
+ */
+public class RunMain {
+    public static void main(String[] args) {
+        System.out.println("main start->");
+    }
+}
