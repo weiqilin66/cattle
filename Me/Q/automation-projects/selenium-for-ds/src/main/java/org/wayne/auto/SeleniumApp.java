14d1e575bce4e03db9d4d241ae09f58e39d8432d
weiqilin66 <weiqilin66@gmail.com>
2021-05-30 21:21:36 +0800

    selenium 基本环境

---
+++ Me/Q/automation-projects/selenium-for-ds/src/main/java/org/wayne/auto/SeleniumApp.java
@@ -0,0 +1,22 @@
+package org.wayne.auto;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * @TODO:
+ * @author: lwq
+ */
+public class SeleniumApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\selenium_driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.itest.info");
+
+        String title = driver.getTitle();
+        System.out.printf(title);
+
+        driver.close();
+    }
+}
+
