1dcefc8d1683d2abd1233c1e55e7d7a36b46afa4
weiqilin66 <weiqilin66@gmail.com>
2021-06-27 21:49:19 +0800

    分割

--- Me/Q/projects/base-server/src/main/java/com/lwq/hr/HrApplication.java
+++ Me/Q/projects/base-server/src/main/java/com/lwq/hr/HrApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.lwq.hr.mapper")
-// 开启springCache
 @EnableCaching
 public class HrApplication {
 
